f34efe17dc5c2bdd6faaa39fb23479e9d7338d44
John Ho <ho.john.nguyen@gmail.com>
2023-11-04 08:33:01 +0100

    Created a new api endpoint.

---
+++ ClientApp/src/components/HelloWorld.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+export class HelloWorld extends Component {
+    static displayName = HelloWorld.name;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            messages: null,
+            loading: true
+        }
+            ;
+    }
+
+    componentDidMount() {
+        this.populateData();
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : HelloWorld.renderMessage(this.state.messages);
+
+        return (
+            <div>
+                <h1 id="tableLabel">My first api call!</h1>
+                {contents}
+            </div>
+        );
+    }
+
+    async populateData() {
+        const response = await fetch('helloworld');
+        const data = await response.json();
+        this.setState({ messages: data, loading: false });
+    }
+
+    static renderMessage(messages) {
+        return (
+            <div>
+                <div>The following message is coming from api/HelloWorld</div>
+                {messages.map(message => 
+                    <div>
+                        <span>{message}</span>
+                        <span>, </span>
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace learningReactWithAspDotNet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        private readonly ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+        /* https://learn.microsoft.com/en-us/aspnet/core/web-api/action-return-types?view=aspnetcore-7.0 */
+        [HttpGet]
+        public IEnumerable<String> Get() => new List<String> { "John", "Says", "Hi"};
+    }
+}
